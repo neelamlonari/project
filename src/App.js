587767e758519461001f26a6f4100155115c02e0
neelamlonari <neelam.lonari@hotmail.com>
2023-02-10 14:29:09 -0500

    Location added

--- src/App.js
+++ src/App.js
@@ -6,14 +6,12 @@ const api ={
   
   key: "35b5991713a9a5059442e37f0ef572be",
   base:"https://api.openweathermap.org/data/2.5/",
-  lat :"35.2272086",
-  lon:"-80.8430827"
 
 }
 
 
 function App() {
-  
+
   const date =()=>{
     let options={year:"numeric",month:"long",day:"numeric"}
   
@@ -22,6 +20,20 @@ function App() {
       return `${date}`;
   }
  
+  const [query,setQuery]= useState('');
+  const [weather,setWeather]= useState({});
+
+  const search = evt =>{
+    if(evt.key === "Enter"){
+      fetch(`${api.base}/weather?q=${query}&appid=${api.key}`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+        console.log(result);
+      })
+      }
+    }
   
  
   return (
@@ -31,6 +43,9 @@ function App() {
           <input type ="text"
                  className="search-bar"
                  placeholder="search..."
+                 onChange = {e=>setQuery(e.target.value)}
+                 value = {query}
+                 onKeyUp = {search}
                  />
           <div className="Location-box">
             <div className="Location">charlotte</div>
